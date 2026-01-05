7dfe807fb25f47af66e6c5117ad66283b53e62a2
BusraKahraman <busrakahraman.b@gmail.com>
2026-01-05 16:29:39 +0300

    Initial Next.js project setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+	return (
+		<main className='p-8'>
+			<h1 className='text-3xl font-bold'>SmartGrocer 🛒</h1>
+			<p className='mt-2 text-gray-600'>
+				Modern grocery shopping demo built with Next.js
+			</p>
+		</main>
+	);
+}
