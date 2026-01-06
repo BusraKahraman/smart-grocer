115187836590b77ad8e8f51cb3af760130165e8d
BusraKahraman <busrakahraman.b@gmail.com>
2026-01-06 19:36:33 +0300

    Add mock product data

---
+++ components/data/products.js
@@ -0,0 +1,20 @@
+export const products = [
+	{
+		id: 1,
+		name: 'Organic Apples',
+		price: 3.99,
+		category: 'Fruits',
+	},
+	{
+		id: 2,
+		name: 'Almond Milk',
+		price: 2.49,
+		category: 'Dairy Alternatives',
+	},
+	{
+		id: 3,
+		name: 'Whole Wheat Bread',
+		price: 1.99,
+		category: 'Bakery',
+	},
+];
